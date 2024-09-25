1ef6ab4758dcd3c88721b3df3b7012e7e7902a6d
Erling <erlingkris97@hotmail.com>
2024-09-25 22:22:27 +0200

    Søk av enheter

---
+++ Nettside/script.js
@@ -0,0 +1,5 @@
+// Funksjon for å vise tilgjengelige IoT-enheter
+document.getElementById('searchDevicesButton').addEventListener('click', function() {
+    // Vis listen med enheter når søket starter
+    document.getElementById('devicesList').style.display = 'block';
+});
