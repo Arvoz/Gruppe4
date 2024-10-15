7435015f43eb5def196dcd9d0b676389bae26f93
Erling <erlingkris97@hotmail.com>
2024-10-15 19:11:33 +0200

    GUI - fix

--- gui/SimpleGUIApp/Form1.cs
+++ gui/SimpleGUIApp/Form1.cs
@@ -9,41 +9,22 @@ namespace SimpleGUIApp
         private Button secondButton;
         private TextBox screenTextBox;
         private TrackBar myTrackBar;
+        private Label sliderLabel;
+        private GroupBox buttonGroup;
+        private GroupBox sliderGroup;
 
         public Form1()
         {
-            // Initialize form and controls
             this.Text = "Dynamic Resizing Example";
-            this.Size = new System.Drawing.Size(600, 400);  // Initial window size
-            this.MinimumSize = new System.Drawing.Size(400, 300);  // Minimum form size
+            this.Size = new System.Drawing.Size(600, 500);  // Initial window size
+            this.MinimumSize = new System.Drawing.Size(500, 400);  // Minimum form size
 
-            SetupButton();
-            SetupSecondButton();
+            SetupButtonGroup();
+            SetupSliderGroup();
             SetupScreen();
-            SetupTrackBar();
 
             // Enable dynamic resizing for controls
             this.Resize += new EventHandler(Form1_Resize);
-        }
-
-        // Setup the first button
-        private void SetupButton()
-        {
-            myButton = new Button();
-            myButton.Text = "Press Me";
-            myButton.Size = new System.Drawing.Size(100, 50);
-            myButton.Location = new System.Drawing.Point(50, 50);
-            Controls.Add(myButton);
-        }
-
-        // Setup the second button
-        private void SetupSecondButton()
-        {
-            secondButton = new Button();
-            secondButton.Text = "Second Button";
-            secondButton.Size = new System.Drawing.Size(100, 50);
-            secondButton.Location = new System.Drawing.Point(200, 50);
-            Controls.Add(secondButton);
         }
 
         // Setup the screen (TextBox)
@@ -54,71 +35,99 @@ namespace SimpleGUIApp
             screenTextBox.ReadOnly = true;
             screenTextBox.Text = "Screen Output";
             screenTextBox.Font = new System.Drawing.Font("Consolas", 12);
-            screenTextBox.Location = new System.Drawing.Point(50, 150);
+            screenTextBox.Location = new System.Drawing.Point(50, 300);
             screenTextBox.Size = new System.Drawing.Size(500, 100);  // Initial size
             Controls.Add(screenTextBox);
         }
 
-        // Setup the TrackBar (Slider)
-        private void SetupTrackBar()
+        // Setup button group
+        private void SetupButtonGroup()
         {
+            buttonGroup = new GroupBox();
+            buttonGroup.Text = "Buttons";
+            buttonGroup.Size = new System.Drawing.Size(350, 100);
+            buttonGroup.Location = new System.Drawing.Point(50, 50);
+
+            myButton = new Button();
+            myButton.Text = "Button One";
+            myButton.Size = new System.Drawing.Size(100, 50);
+            myButton.Location = new System.Drawing.Point(10, 30);
+            myButton.Click += new EventHandler(MyButton_Click);
+            buttonGroup.Controls.Add(myButton);
+
+            secondButton = new Button();
+            secondButton.Text = "Button Two";
+            secondButton.Size = new System.Drawing.Size(100, 50);
+            secondButton.Location = new System.Drawing.Point(150, 30);
+            secondButton.Click += new EventHandler(SecondButton_Click);
+            buttonGroup.Controls.Add(secondButton);
+
+            Controls.Add(buttonGroup);
+        }
+
+        // Setup slider group
+        private void SetupSliderGroup()
+        {
+            sliderGroup = new GroupBox();
+            sliderGroup.Text = "Slider Control";
+            sliderGroup.Size = new System.Drawing.Size(350, 120);
+            sliderGroup.Location = new System.Drawing.Point(50, 160);
+
+            sliderLabel = new Label();
+            sliderLabel.Text = "Slider value: 1";
+            sliderLabel.Font = new System.Drawing.Font("Arial", 10);
+            sliderLabel.Location = new System.Drawing.Point(10, 20);
+            sliderLabel.AutoSize = true;
+            sliderGroup.Controls.Add(sliderLabel);
+
             myTrackBar = new TrackBar();
             myTrackBar.Minimum = 1;
             myTrackBar.Maximum = 12;
             myTrackBar.Value = 1;
             myTrackBar.TickFrequency = 1;
-            myTrackBar.Size = new System.Drawing.Size(500, 45);  // Initial size
-            myTrackBar.Location = new System.Drawing.Point(50, 300);
-            Controls.Add(myTrackBar);
+            myTrackBar.Size = new System.Drawing.Size(300, 45);
+            myTrackBar.Location = new System.Drawing.Point(10, 50);
+            myTrackBar.Scroll += new EventHandler(TrackBar_Scroll);
+            sliderGroup.Controls.Add(myTrackBar);
+
+            Controls.Add(sliderGroup);
+        }
+
+        private void TrackBar_Scroll(object sender, EventArgs e)
+        {
+            sliderLabel.Text = "Slider value: " + myTrackBar.Value.ToString();
+            UpdateSliderValueOnScreen();
+        }
+
+        private void UpdateSliderValueOnScreen()
+        {
+            string[] lines = screenTextBox.Lines;
+            if (lines.Length > 0 && lines[lines.Length - 1].StartsWith("Slider value:"))
+            {
+                screenTextBox.Lines = lines[0..^1];  // Remove last line
+            }
+            UpdateScreen("Slider value: " + myTrackBar.Value.ToString());
+        }
+
+        private void UpdateScreen(string message)
+        {
+            screenTextBox.AppendText(Environment.NewLine + message);
         }
 
         // Event handler for dynamically resizing the controls
         private void Form1_Resize(object sender, EventArgs e)
         {
-            // Get current form size
-            int formWidth = this.ClientSize.Width;
-            int formHeight = this.ClientSize.Height;
-
-            // Dynamically resize and reposition controls based on form size
-            myButton.Location = new System.Drawing.Point(formWidth / 20, formHeight / 20);
-            myButton.Size = new System.Drawing.Size(formWidth / 6, formHeight / 10);
-
-            secondButton.Location = new System.Drawing.Point(formWidth / 5, formHeight / 20);
-            secondButton.Size = new System.Drawing.Size(formWidth / 6, formHeight / 10);
-
-            screenTextBox.Location = new System.Drawing.Point(formWidth / 20, formHeight / 3);
-            screenTextBox.Size = new System.Drawing.Size(formWidth - (formWidth / 10), formHeight / 5);
-
-            myTrackBar.Location = new System.Drawing.Point(formWidth / 20, formHeight - (formHeight / 5));
-            myTrackBar.Size = new System.Drawing.Size(formWidth - (formWidth / 10), 45);  // TrackBar fixed height, dynamic width
+            screenTextBox.Width = this.ClientSize.Width - 100;  // Adjust dynamically
         }
 
-        // Event handler for the first button click
         private void MyButton_Click(object sender, EventArgs e)
         {
-            screenTextBox.Text = "Button has been pressed";
+            UpdateScreen("Button has been pressed");
         }
 
-        // Event handler for the second button click
         private void SecondButton_Click(object sender, EventArgs e)
         {
-            screenTextBox.Text = "Second button has been pressed";
-        }
-
-        // Event handler for TrackBar (Slider)
-        private void TrackBar_Scroll(object sender, EventArgs e)
-        {
-            screenTextBox.Text = "Slider value: " + myTrackBar.Value.ToString();
-        }
-
-        // Simplified Dispose method
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                // Dispose of any other resources if necessary (leave empty for now)
-            }
-            base.Dispose(disposing);
+            UpdateScreen("Second button has been pressed");
         }
     }
 }
